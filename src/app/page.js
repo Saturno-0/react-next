7ee49e555c8b04f8dbbc6b72fdc9adf0dfc91df0
Saturno-0 <rst020514@gmail.com>
2024-02-13 09:10:01 -0600

    jjjjjj

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Pagina principal
+      <Link classname href={"/profile"}/>Tacos de guebo<Link/> 
+    </div>
+  )
+}
+
+export default page 
